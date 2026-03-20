97ba2900296b718ad22cb6def1ada8d0955ae60d
gonnabethebestcoder <aaadubey2003@gmail.com>
2026-03-20 10:21:51 +0400

    data validation completed

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -30,3 +30,9 @@ class DataIngestionConfig:  #configuration values-->configured in constants.py
     train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO   #0.25
     collection_name:str = DATA_INGESTION_COLLECTION_NAME    #"Proj1-Data"
 #This method of TrainPipeline class is responsible for starting data ingestion component
+
+@dataclass 
+class DataValidationConfig:
+    data_validation_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_VALIDATION_DIR_NAME)
+    validation_report_file_path = os.path.join(data_validation_dir, DATA_VALIDATION_REPORT_FILE_NAME)   #report.yaml
+#this method is validates the data by creating a report.yaml file in the data_validation_dir 
