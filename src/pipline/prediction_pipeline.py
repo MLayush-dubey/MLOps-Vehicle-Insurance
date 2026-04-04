d63dca419f45d7797e3c2cd226cc10e9eb1c3870
MLayush-dubey <aadubey1106@gmail.com>
2026-04-04 09:13:35 +0400

    added simple frontend and backend for serving predictions

---
+++ src/pipline/prediction_pipeline.py
@@ -0,0 +1,115 @@
+import sys 
+from src.entity.config_entity import VehiclePredictorConfig
+from src.entity.s3_estimator import Proj1Estimator 
+from src.exception import MyException 
+from src.logger import logging
+from pandas import DataFrame 
+
+#this class is the data container-->individual input variables leta hai aur usko df meh convert krta h for model pred
+class VehicleData:
+    def __init__(self,
+                Gender,
+                Age,
+                Driving_License,
+                Region_Code,
+                Previously_Insured,
+                Annual_Premium,
+                Policy_Sales_Channel,
+                Vintage,
+                Vehicle_Age_lt_1_Year,
+                Vehicle_Age_gt_2_Years,
+                Vehicle_Damage_Yes
+                ):
+        """
+        Vehicle Data constructor 
+        Input: all features of the trained model for prediction
+        """
+        #current input ko save krle rahe h for a particular user
+        try:
+            self.Gender = Gender 
+            self.Age = Age 
+            self.Driving_License = Driving_License
+            self.Region_Code = Region_Code 
+            self.Previously_Insured = Previously_Insured 
+            self.Annual_Premium = Annual_Premium 
+            self.Policy_Sales_Channel = Policy_Sales_Channel 
+            self.Vintage = Vintage 
+            self.Vehicle_Age_lt_1_Year = Vehicle_Age_lt_1_Year 
+            self.Vehicle_Age_gt_2_Years = Vehicle_Age_gt_2_Years 
+            self.Vehicle_Damage_Yes = Vehicle_Damage_Yes 
+
+        except Exception as e:
+            raise MyException(e, sys)
+        
+
+#jo save kiya hua h constructor meh, ussiko humlog dataframe meh convert krde rahe hai
+    def get_vehicle_input_data_frame(self) -> DataFrame:
+        """
+        This function returns a dataframe
+        """
+        try:
+            vehicle_input_dict = self.get_vehicle_data_as_dict()  #niche vale func ko call kr raha hai
+            return DataFrame(vehicle_input_dict)
+        
+        except Exception as e:
+            raise MyException(e, sys) from e 
+        
+#ye main self values ko lekar dictionary meh convert krta h aur fir uppar method ko bhejta h to convert to dataframe
+    def get_vehicle_data_as_dict(self):
+        """
+        This function returns a dictionary from VehicleData class input
+        """
+        logging.info("Entered the get_vehicle_data_as_dict method of VehicleData class")
+#since pandas expects inputs in the form of list/array to convert to dataframe, hence we passed in []
+        try:
+            input_data = {
+                "Gender": [self.Gender],
+                "Age": [self.Age],
+                "Driving_License": [self.Driving_License],
+                "Region_Code": [self.Region_Code],
+                "Previously_Insured": [self.Previously_Insured],
+                "Annual_Premium": [self.Annual_Premium],
+                "Policy_Sales_Channel": [self.Policy_Sales_Channel],
+                "Vintage": [self.Vintage],
+                "Vehicle_Age_lt_1_Year": [self.Vehicle_Age_lt_1_Year],
+                "Vehicle_Age_gt_2_Years": [self.Vehicle_Age_gt_2_Years],
+                "Vehicle_Damage_Yes": [self.Vehicle_Damage_Yes]
+            }
+
+            logging.info("Created vehicle data dict")
+            logging.info("Exited the get_vehicle_data_as_dict method as VehicleData class")
+            return input_data 
+        
+        except Exception as e:
+            raise MyException(e, sys)
+
+
+#delivery service class--> looks for the model in s3, uses proj1estimator to pull that model        
+class VehicleDataClassifier:
+    def __init__(self,prediction_pipeline_config: VehiclePredictorConfig = VehiclePredictorConfig(),) -> None:
+        """
+        :param prediction_pipeline_config: Configuration for prediction the value
+        """
+        try:
+            self.prediction_pipeline_config = prediction_pipeline_config
+        except Exception as e:
+            raise MyException(e, sys) 
+        
+    
+    def predict(self, dataframe) -> str:
+        """
+        This is the method of VehicleDataClassifier
+        Returns: Prediction in string format
+        """
+        try:
+            logging.info("Entered predict method of VehicleDataClassifier class")
+            model = Proj1Estimator(
+                bucket_name = self.prediction_pipeline_config.bucket_name,
+                model_path = self.prediction_pipeline_config.model_file_path
+            )
+            result = model.predict(dataframe)  #proj1estimator class k andhar ka method
+
+            return result 
+        
+        except Exception as e:
+            raise MyException(e, sys)        
